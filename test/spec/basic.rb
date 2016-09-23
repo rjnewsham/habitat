414ab16680b5c6f8e9bd559ec9a66553f3e6dd3c
Dave Parfitt <dparfitt@chef.io>
2016-09-23 13:31:59 -0400

    wip Signed-off-by: Dave Parfitt <dparfitt@chef.io>

--- test/spec/basic.rb
+++ test/spec/basic.rb
@@ -63,6 +63,8 @@ describe "Habitat CLI" do
             # upon success
             ctx.register_dir "results"
 
+            `sed -i '23iecho "TACOS: $\{hab\}"' $(find /hab -name hab-studio-type-default.sh -type f)`
+
             # building a package can take quite awhile, let's bump the timeout to
             # 60 seconds to be sure we finish in time.
             result = ctx.cmd_expect("studio build fixtures/simple_service",
